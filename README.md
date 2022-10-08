c7905d70e9da63344badd91ad43e95d5c04d3802
EllisJLC <jia.liang.chen@ryerson.ca>
2022-10-08 00:34:43 -0400

    final edits and README.md file

--- README.md
+++ README.md
@@ -1,11 +1,10 @@
-# yes
-
+# README.md Generator
 
 ## Introduction
 
-yes
+Node.js/JavaScript/Inquirer application to accept user input and generate professional README.md document.
 
-## Table of Contents
+## Table of Contents 
 1. [Installation](#installation)
 2. [Usage](#usage)
 3. [Test](#test)
@@ -15,29 +14,29 @@ yes
 
 ## Installation 
 
-yesy
+Install program through cloning on Github using clone function (git clone https://github.com/EllisJLC/README_Gen.git).
 
 ## Usage
 
-yesy
+Used for efficient generation of README.md files including necessary information and professional formatting
 
 ## Test
 
-undefined## Images 
-### Image of wireframe plan: 
+1. Run code using "node index.js" command in node.
+2. Input requested information.
+3. Submitted information will be used to create README.md file, confirmed when File saved message is logged.
 
+## Images
 
-![Wireframe](./assets/images/wireframe.png)
+Image of console
 
+![Console view of inquiries made by Inquirer](./assets/images/preview.png)
 
-### Image of working site:
-
-
-![Program sample](./assets/images/preview.png)
 ## Credits
 
+All work was completed by Jia Liang (Ellison) Chen.
 
+## Licenses 
+![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
 
-## Licenses
-
-s
+The MIT License: https://opensource.org/licenses/MIT
--- utils/writer.js
+++ utils/writer.js
@@ -1,10 +1,10 @@
 module.exports = function writing (data) {
-  let {title, introduction, installation, usage, test, image, credits, license} = data;
+  let {title, introduction, installation, usage, test, image, wireframe, credits, license} = data;
   let i = 1;
-  let written = `# ${title}\n
+  let written = `# ${title}
 \n## Introduction
 \n${introduction}
-\n## Table of Contents
+\n## Table of Contents 
 ${i, i++}. [Installation](#installation)
 ${i, i++}. [Usage](#usage)
 ${i, i++}. [Test](#test)\n`;
@@ -18,51 +18,56 @@ ${i, i++}. [Licenses](#licenses)
 \n## Usage
 \n${usage}
 \n## Test
-\n${test}`
-if (image === "yes") {
-  written += `## Images \n### Image of wireframe plan: \n
-![Wireframe](./assets/images/wireframe.png)\n
-### Image of working site:\n
+\n${test}\n`
+if (wireframe === "yes" || image === "yes") {
+  written += "\n## Images"
+  if (wireframe === "yes") {
+    written += `\n### Image of wireframe plan: \n
+![Wireframe](./assets/images/wireframe.png)\n`
+  }
+  if (image === "yes") {
+`\n### Image of working site:\n
 ![Program sample](./assets/images/preview.png)\n`;
+  }
 }
 written += `\n## Credits
 \n${credits}
-\n## Licenses`;
+\n## Licenses \n`;
 switch (license) {
   case "apache":
-    written += `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)
+    written += `![License](https://img.shields.io/badge/License-Apache_2.0-blue.svg)\n
 Apache 2.0 License https://opensource.org/licenses/Apache-2.0`;
     break;
   case "boost":
-    written += `[License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)
+    written += `[License](https://img.shields.io/badge/License-Boost_1.0-lightblue.svg)\n
 Boost Software License 1.0: https://www.boost.org/LICENSE_1_0.txt`;
     break;
   case "eclipse":
-      written += `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)
+      written += `![License](https://img.shields.io/badge/License-EPL_1.0-red.svg)\n
 Eclipse Public License 1.0: https://opensource.org/licenses/EPL-1.0`;
     break;
   case "ibm":
-      written += `![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)
+      written += `![License: IPL 1.0](https://img.shields.io/badge/License-IPL_1.0-blue.svg)\n
 IBM Public License Version 1.0: https://opensource.org/licenses/IPL-1.0`;
   break;
   case "isc":
-    written += `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)
+    written += `![License: ISC](https://img.shields.io/badge/License-ISC-blue.svg)\n
 ISC License (ISC): https://opensource.org/licenses/ISC`;
   break;
   case "mit":
-    written += `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)
+    written += `![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)\n
 The MIT License: https://opensource.org/licenses/MIT`;
   break;
   case "mozilla":
-    written += `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)
+    written += `![License: MPL 2.0](https://img.shields.io/badge/License-MPL_2.0-brightgreen.svg)\n
 Mozilla Public License 2.0: https://opensource.org/licenses/MPL-2.0`;
   break;
-  case "WTFPL":
-    written += `![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)
+  case "wtfpl":
+    written += `![License: WTFPL](https://img.shields.io/badge/License-WTFPL-brightgreen.svg)\n
 The Do What the F*ck You Want to Public License: http://www.wtfpl.net/about/`;
   break;
-  case "Zlib":
-    written += `![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)
+  case "zlib":
+    written += `![License: Zlib](https://img.shields.io/badge/License-Zlib-lightgrey.svg)\n
 The zlib/libpng License: https://opensource.org/licenses/Zlib`;
   break;
   default:
